92037fe80a2da247094c682306428725d0fafeb4
Geoffray-buhler <geoffray.buhler@gmail.com>
2020-08-31 19:56:25 +0200

    first commit

---
+++ precache-manifest.f8c52968bd385155e444f3281eca33d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83842b6bb13d1afd925cc0f262d95528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c192a0d7d9e998d73e9",
+    "url": "/static/css/main.475d9917.chunk.css"
+  },
+  {
+    "revision": "90dd2dccb8811fcbbac1",
+    "url": "/static/js/2.22daef87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22daef87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c192a0d7d9e998d73e9",
+    "url": "/static/js/main.25a14625.chunk.js"
+  },
+  {
+    "revision": "03031250b05634549845",
+    "url": "/static/js/runtime-main.9227c2ae.js"
+  },
+  {
+    "revision": "0f66f2bb7002e9fdc1f6d444c6882f58",
+    "url": "/static/media/fondpixel.0f66f2bb.png"
+  },
+  {
+    "revision": "5f74ef392157f75e3c7b53a8451022e8",
+    "url": "/static/media/fondsombre3.5f74ef39.png"
+  },
+  {
+    "revision": "fdd82659c70bf0b78b0cfe4baf497835",
+    "url": "/static/media/testouillelogo.fdd82659.png"
+  }
+]);
